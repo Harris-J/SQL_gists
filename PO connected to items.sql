38e9dbdcb628bfc46f49bc7e62238a70d1693e4e
Jason Harris <Harris-J@users.noreply.github.com>
2018-05-24 16:59:21 -0600

    Added comments

--- PO connected to items.sql
+++ PO connected to items.sql
@@ -1,6 +1,7 @@
-select po.item#, t.processed, SUBstring(t.call,2,25) as 'call number', t.item_status,t.n_ckos
-from po_line_item po, item_with_title t
-where po.item# = t.item#
-and po. receive_date > 17167
-and po.collection = 'anf'
-order by [call number]
+-- looks up item numbers, titles and call numbers based on a PO numbers
+SELECT po.item#, t.processed, SUBstring(t.call,2,25) as 'call number', t.item_status,t.n_ckos
+FROM po_line_item po, item_with_title t
+WHERE po.item# = t.item#
+AND po. receive_date > 17167
+AND po.collection = 'anf'
+ORDER BY [call number]
